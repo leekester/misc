22bf8fb48e21f10e472a56fe51fbe736eaafbe75
Matt Leeke <mattleeke@hastingsdirect.com>
2022-07-29 16:26:38 +0100

    Adding FronDoor WAF

---
+++ frontDoor/wafComparison.ps1
@@ -0,0 +1,7 @@
+﻿#$fd1 = ""
+#$fd1ResourceGroup = "rg-frontdoor-prd-001"
+
+#$fd1WafPolicies = az network front-door waf-policy list --resource-group rg-frontdoor-prd-001 | ConvertFrom-Json
+ForEach ( $policy in $fd1WafPolicies) {
+    $policy.customRules.rules.Count
+}
